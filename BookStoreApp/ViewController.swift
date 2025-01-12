6c1fbeac12c41a2b3744be50d39c181da34ddaee
Smirnov Sergey <fost@live.ru>
2025-01-12 15:23:50 +0300

    Создан удаленный репозитарий

---
+++ BookStoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookStoreApp
+//
+//  Created by Сергей Смирнов on 12.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupView()
+	}
+
+
+}
+
+extension ViewController {
+	private func setupView() {
+		view.backgroundColor = .black
+	}
+}
+
